df517b804cff8c80275c5fb6eb145956daca9c82
Riedlinger, Nico <nico.riedlinger@gmx.de>
2025-01-14 13:56:00 +0100

    Renamed docker image for keycloak

--- setup/deploy_keycloak.sh
+++ setup/deploy_keycloak.sh
@@ -1,13 +1,13 @@
 # google cloud project settings
-PROJECT_ID=msi-cad-vw-staging
-SERVICE_ACCOUNT=msi-cad-vw-staging-account@$PROJECT_ID.iam.gserviceaccount.com
+PROJECT_ID=msi-cad-vw-parking
+SERVICE_ACCOUNT=cr-deploy@msi-cad-vw-parking.iam.gserviceaccount.com 
 
 # configure keycloak
 KEYCLOAK_VERSION=26.0
 
 # set google cloud project
 gcloud config set project $PROJECT_ID
-gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=./keys/key-msi-cad-vw-staging-account.json
+gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=./keys/msi-cad-vw-parking-e0dfd559021f.json
 
 # pull keycloak image
 #docker pull keycloak/keycloak:$KEYCLOAK_VERSION
@@ -21,13 +21,13 @@ gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=./keys/key-msi-
 #	--service-account $SERVICE_ACCOUNT
 
 
-docker build . --tag msi-cad-vw-keycloak --file github/setup/keycloak.Dockerfile
-docker tag msi-cad-vw-keycloak:latest europe-west1-docker.pkg.dev/$PROJECT_ID/docker-repo/keycloak:latest
-docker push europe-west1-docker.pkg.dev/$PROJECT_ID/docker-repo/keycloak:latest
+docker build . --tag msi-cad-vw-keycloak-cr --file github/setup/keycloak.Dockerfile
+docker tag msi-cad-vw-keycloak-cr:latest europe-west1-docker.pkg.dev/$PROJECT_ID/docker-repo/keycloak-cr:latest
+docker push europe-west1-docker.pkg.dev/$PROJECT_ID/docker-repo/keycloak-cr:latest
 
 # use argument --update-env-vars to create a temporary admin account for first login
 # --update-env-vars=KC_BOOTSTRAP_ADMIN_USERNAME=XXX,KC_BOOTSTRAP_ADMIN_PASSWORD=XXX
-gcloud run deploy msi-cad-vw-keycloak --image=europe-west1-docker.pkg.dev/$PROJECT_ID/docker-repo/keycloak:latest \
+gcloud run deploy msi-cad-vw-keycloak --image=europe-west1-docker.pkg.dev/$PROJECT_ID/docker-repo/keycloak-cr:latest \
     --platform=managed \
 	--region=europe-west1 \
 	--port=8080 \
--- setup/keycloak.Dockerfile
+++ setup/keycloak.Dockerfile
@@ -10,10 +10,13 @@ RUN /opt/keycloak/bin/kc.sh build
 FROM quay.io/keycloak/keycloak:latest
 COPY --from=builder /opt/keycloak/ /opt/keycloak/
 
-ENV KC_HOSTNAME=https://msi-cad-vw-keycloak-555986620136.europe-west1.run.app
+ENV KC_HOSTNAME=https://msi-cad-vw-keycloak-768187275150.europe-west1.run.app
 ENV KC_HOSTNAME_STRICT=false
 ENV KC_HTTP_ENABLED=true
 ENV KC_CACHE=local
+
+ENV KEYCLOAK_ADMIN=admin
+ENV KEYCLOAK_ADMIN_PASSWORD=adminpassword
 
 ENTRYPOINT ["/opt/keycloak/bin/kc.sh"]
 CMD ["start"]
